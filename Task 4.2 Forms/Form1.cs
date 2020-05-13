469e9a2f16f4579d78d83b46ac6258e82882717b
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-13 15:18:12 +0300

    .

--- Task 4.2 Forms/Form1.cs
+++ Task 4.2 Forms/Form1.cs
@@ -12,9 +12,33 @@ namespace Task_4._2_Forms
 {
     public partial class Form1 : Form
     {
+        public static string s = "";
         public Form1()
         {
             InitializeComponent();
+        }
+        public static void Rec(int n)
+        {
+            if (n > 0)
+             {
+                s += n + " ";
+                 Rec(n - 1);
+             }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+                int n = int.Parse(textBox1.Text);
+                if (n == 0)
+                textBox2.Text += "Ошибка: N = 0";
+                else if (n < 0)
+                textBox2.Text += "Ошибка: N меньше 0";
+                else if (n > 1000)
+                textBox2.Text += "Ошибка: N больше 1000";
+                else
+                    Rec(n);
+            textBox2.Text = s;
+            s = "";
         }
     }
 }
